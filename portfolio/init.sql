987b9d9a289c9547b979d77ccc3949d9eb482a89
shashank.p <shashank.p@npci.org.in>
2023-07-26 12:11:26 +0530

    scripts updated for portfolio setup

--- portfolio/init.sql
+++ portfolio/init.sql
@@ -2,6 +2,9 @@
 
 -- noinspection SqlNoDataSourceInspectionForFile
 
+-- CREATE DATABASE db;
+-- DROP DATABASE db;
+
 CREATE TABLE users (
                        name VARCHAR(100),
                        email VARCHAR(100),
@@ -15,10 +18,12 @@ CREATE TABLE passwords (
                            salt VARCHAR(100)
 );
 
-INSERT INTO passwords (password, salt) VALUES ('adminpass', 'randomsalt');
+INSERT INTO passwords (password, salt) VALUES ('adminpass', 'salt');
 
 SET @admin_password_id = LAST_INSERT_ID();
 
 INSERT INTO users (name, email, username, password_id) VALUES ('Admin', 'admin@example.com', 'admin', @admin_password_id);
 
 -- UPDATE passwords SET password = 'adminpass123' WHERE password_id = 1;
+-- SELECT * FROM passwords;
+-- SELECT * FROM users;
